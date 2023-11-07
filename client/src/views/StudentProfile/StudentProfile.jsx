6bdb327039bc6ec4383297784f5dab481d4196fa
matthewself12 <matthewself12@gmail.com>
2023-11-06 21:29:56 -0500

    Implemented Badge Collection, Re-Dumped database

--- client/src/views/StudentProfile/StudentProfile.jsx
+++ client/src/views/StudentProfile/StudentProfile.jsx
@@ -5,24 +5,36 @@ import 'react-tabs/style/react-tabs.less';
 import { useNavigate } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar';
 import { useGlobalState } from '../../Utils/userState';
-import { getCurrentStudents, getStudent, getStudentClassroom } from '../../Utils/requests';
+import { getCurrentStudents, getStudents, getStudentClassroom } from '../../Utils/requests';
 import './StudentProfile.less';
-import './BadgeList.jsx'
+import './BadgeList.jsx';
 
 function StudentProfile() {
 
-  const [selectedTab, setSelectedTab] = useState(0);
   const [currentStudent, setCurrentStudent] = useState();
-  const [editMode, setEditMode] = useState(false)
+  const [badgesArr, setBadgesArr] = useState([]);  
+  const [editMode, setEditMode] = useState(false);
 
-  // Get the currently logged in student
-  getCurrentStudents().then((res) => {
+  // Get the currently logged in student (if not already retrieved)
+  if(!currentStudent) {
+    getCurrentStudents().then((res) => {
       if (res.data) {
         setCurrentStudent(res.data.students[0]);
+        console.log(res.data);
+        // parse the badges from the student object
+        const badgesObj = res.data.students[0].badges;
+        var badgesArr = [];
+        badgesObj.forEach(function (currentVal) {
+          badgesArr.push(currentVal.name);
+        });
+        setBadgesArr(badgesArr);
       } else {
         message.error(res.err);
       }
     });
+  }
+
+  console.log(badgesArr);
 
   return (
     <div className='container nav-padding'>
@@ -41,7 +53,7 @@ function StudentProfile() {
               Featured Projects Go Here
             </TabPanel>
             <TabPanel>
-              {/* BadgeList(currentStudent.id, editMode) */}
+              <div>{!(currentStudent == null) ? badgesArr.map((name) => <p>{name}</p>) : ''}</div>
             </TabPanel>
           </Tabs>
         </div>
