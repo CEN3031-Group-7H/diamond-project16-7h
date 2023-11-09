5f71117d4aebd4ea5a0d80fb1cc39494c7126535
EfrenL0pez <93171229+EfrenL0pez@users.noreply.github.com>
2023-11-08 21:40:31 -0700

    created updateVisibility function for badge

--- server/api/badge/controllers/badge.js
+++ server/api/badge/controllers/badge.js
@@ -5,4 +5,17 @@
  * to customize this controller
  */
 
-module.exports = {};
+module.exports = {
+  
+    // This is the updateVisibility function.
+    updateVisibility: async ctx => {
+  
+      const { id, visible } = ctx.request.body;
+  
+     
+      const updatedBadge = await strapi.services.badge.update({ id }, { visible });
+  
+    
+      return updatedBadge;
+    },
+  };
