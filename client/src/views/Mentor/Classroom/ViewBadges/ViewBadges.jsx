2bd2574e766b3591cc3fe2f40379233607465f04
Thomas Chang <tlchang02@gmail.com>
2023-11-08 21:28:56 -0500

    Edits to button positioning.
    Made a temporary edit to make buttons look better. Currently WIP for sticky buttons which will stay on the screen after scrolling a specified distance.

--- client/src/views/Mentor/Classroom/ViewBadges/ViewBadges.jsx
+++ client/src/views/Mentor/Classroom/ViewBadges/ViewBadges.jsx
@@ -1,5 +1,5 @@
 import './ViewBadges.less';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MentorSubHeader from '../../../../components/MentorSubHeader/MentorSubHeader';
 import { useNavigate } from 'react-router-dom';
 import createButton from './images/create.png';
@@ -7,17 +7,21 @@ import removeButton from './images/remove.png';
 
 function ViewBadges() {
   const navigate = useNavigate();
+
+  // This is used when the add badge button is clicked
   const handleAddBadge = () => {
+    // Change url to badge creator
     navigate('/classroom/10?tab=BadgeCreator');
+    // Refresh page
     window.location.reload();
   }
-  
+
     return (
         <div className="badgePane">
           <MentorSubHeader
             title={'ViewBadges'}
           />
-          <div className="view-badge-container">
+          <div className={"view-badge-container"}>
             <button
               id='add-new-button'
               onClick={handleAddBadge}
@@ -29,7 +33,7 @@ function ViewBadges() {
               id='remove-button'
               // Perhaps edit mode? Or maybe drag and drop feature?
             >
-              <img src={removeButton} alt="Add new badge"/>
+              <img src={removeButton} alt="Edit/Remove badge"/>
             </button>
           </div>
         </div>
